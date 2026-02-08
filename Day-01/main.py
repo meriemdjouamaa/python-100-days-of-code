8395154372ea2523578ce6d3fdd8dbe67dfddf02
Meriem <223598822+stacked-ai-dev@users.noreply.github.com>
2026-02-08 16:54:29 +0000

    Day 1: username generator project

---
+++ Day-01/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Username Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your username could be " + city + " " + pet)
