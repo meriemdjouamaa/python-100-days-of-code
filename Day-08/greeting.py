0fbb756cfbbeb7f4ee634c30f2010c6422f35d94
Meriem Djouamaa <mrmdjouamaa@gmail.com>
2026-02-14 21:27:18 +0000

    Day 8: Simple greeting function

---
+++ Day-08/greeting.py
@@ -0,0 +1,8 @@
+# Simple Function that packages code into a named block
+def greet():
+    print("Hello Angela")
+    print("How do you do Jack Bauer?")
+    print("Isn't the weather nice?")
+
+
+greet()
