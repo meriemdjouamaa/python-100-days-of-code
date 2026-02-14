a107eb1c4da8f765d7aee47c6951c5ce015c1409
Meriem Djouamaa <mrmdjouamaa@gmail.com>
2026-02-14 21:29:08 +0000

    Day 8: Greeting function with inputs

--- Day-08/greeting.py
+++ Day-08/greeting.py
@@ -4,5 +4,12 @@ def greet():
     print("How do you do Jack Bauer?")
     print("Isn't the weather nice?")
 
-
 greet()
+
+# Function that allows for inputs
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+
+greet_with_name("Billie")
