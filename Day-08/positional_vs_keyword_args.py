e3fa6b1a49454380d76c67a2f3cc8125eadd709b
Meriem Djouamaa <mrmdjouamaa@gmail.com>
2026-02-15 07:08:19 +0000

    day 8: Call a function using keyword arguments

--- Day-08/positional_vs_keyword_args.py
+++ Day-08/positional_vs_keyword_args.py
@@ -6,3 +6,12 @@ def greet_with(name, location):
     print(f"What is it like in {location}?")
 
 greet_with("Jack Bauer", "Nairobi")
+
+# Using Keyword Arguments
+def greet_with(name, location):
+    
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")    
+
+
+    greet_with(location="Nairobi", name="Jack Bauer")
