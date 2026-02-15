2476de1ee9f1393a16fa92ec9bd52b1036bf6925
Meriem Djouamaa <mrmdjouamaa@gmail.com>
2026-02-15 07:05:56 +0000

    Day 8: Call a function using positional arguments

---
+++ Day-08/positional_vs_keyword_args.py
@@ -0,0 +1,8 @@
+
+# Using Positional Arguments
+def greet_with(name, location):
+    
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Jack Bauer", "Nairobi")
